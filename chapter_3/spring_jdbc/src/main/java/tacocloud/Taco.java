ee9e50aa425391b35c9292f99c337c6604a5ba80
July <Nightmare-000@yandex.ru>
2023-05-27 00:06:16 +0400

    chapter 3 spring data jdbc

---
+++ chapter_3/spring_jdbc/src/main/java/tacocloud/Taco.java
@@ -0,0 +1,32 @@
+package tacocloud;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Table
+public class Taco {
+    @Id
+    private Long id;
+
+    private Date creatrdAt = new Date();
+
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min=1, message = "You must choose at least 1 ingredient")
+    private List<IngredientRef> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient ingredient){
+        this.ingredients.add(new IngredientRef(ingredient.getId()));
+    }
+}
---
+++ chapter_3/spring_jdbc/src/main/java/tacocloud/data/OrderRepository.java
@@ -0,0 +1,7 @@
+package tacocloud.data;
+
+import org.springframework.data.repository.CrudRepository;
+import tacocloud.TacoOrder;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long>{
+}
